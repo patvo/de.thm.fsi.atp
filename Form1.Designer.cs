1ca60c200045d2642d7f6fa440674dec4f6d86ec
patvo <patvo@patvo.de>
2022-02-25 11:34:18 +0100

    new background image for form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -130,7 +130,7 @@ namespace de.thm.fsi.atp
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImage = global::de.thm.fsi.atp.Properties.Resources.OsPlusBG;
+            this.BackgroundImage = global::de.thm.fsi.atp.Properties.Resources.OsPlusBG1;
             this.ClientSize = new System.Drawing.Size(1780, 957);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace de.thm.fsi.atp.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap OsPlusBG1 {
+            get {
+                object obj = ResourceManager.GetObject("OsPlusBG1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
